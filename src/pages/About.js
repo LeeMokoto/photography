0ba3263e9869b6fadcc113fcc0dd16171b686510
Leepo.Mokoto <leepo.mokoto@disraptor.co.za>
2024-02-02 11:07:34 +0200

    clean up about

--- src/pages/About.js
+++ src/pages/About.js
@@ -23,10 +23,7 @@ const About = () => {
               <br />
               <br />
               Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.
+              nisi ut aliquip ex ea commodo consequat. 
             </p>
             <Link className="btn" to={"/portfolio"}>View my work</Link>
           </div>
